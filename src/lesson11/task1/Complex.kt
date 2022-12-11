264d46750b112315f1c2274cf1f698f3ae4df7ae
andre <andrew6gog@gmail.com>
2022-12-11 19:31:51 +0300

    partly done lesson11

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -5,7 +5,10 @@ package lesson11.task1
 /**
  * Фабричный метод для создания комплексного числа из строки вида x+yi
  */
-fun Complex(s: String): Complex = TODO()
+fun Complex(s: String): Complex {
+    val numbers = s.replace("-", "+-").split("+")
+    return Complex(numbers[0].toDouble(), numbers[1].removeSuffix("i").toDouble())
+}
 
 /**
  * Класс "комплексное число".
@@ -21,40 +24,53 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, .0)
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex = Complex(this.re + other.re, this.im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(-this.re, -this.im)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = this + (-other)
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex =
+        Complex(this.re * other.re - this.im * other.im, this.im * other.re + this.re * other.im)
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex {
+        val conj = Complex(other.re, -other.im)
+        val numer = this * conj
+        val denom = (other * conj).re
+        return Complex(numer.re / denom, numer.im / denom)
+    }
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is Complex &&
+            this.re == other.re &&
+            this.im == other.im
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String =
+        "${this.re}${if (this.im >= 0) "+" else "-"}${this.im}"
+
+    override fun hashCode(): Int {
+        return this.re.hashCode() + 7 * this.im.hashCode()
+    }
 }
--- src/lesson11/task1/DimensionalValue.kt
+++ src/lesson11/task1/DimensionalValue.kt
@@ -2,6 +2,8 @@
 
 package lesson11.task1
 
+import kotlin.math.abs
+
 /**
  * Класс "Величина с размерностью".
  *
@@ -17,61 +19,99 @@ package lesson11.task1
  * - во всех остальных случаях следует бросить IllegalArgumentException
  */
 class DimensionalValue(value: Double, dimension: String) : Comparable<DimensionalValue> {
+
+    private val multiplier = if (dimension.length == 1) 1.0 else when (dimension[0]) {
+        'K' -> 1000.0
+        'm' -> 0.001
+        else -> throw IllegalStateException()
+    }
+
+    private val rawValue = value
+
+    private val rawDimension = dimension
+
     /**
      * Величина с БАЗОВОЙ размерностью (например для 1.0Kg следует вернуть результат в граммах -- 1000.0)
      */
-    val value: Double get() = TODO()
+    val value: Double get() = rawValue * multiplier
 
     /**
      * БАЗОВАЯ размерность (опять-таки для 1.0Kg следует вернуть GRAM)
      */
-    val dimension: Dimension get() = TODO()
+    val dimension: Dimension
+        get() =
+            if (rawDimension.length == 1)
+                when (rawDimension) {
+                    "m" -> Dimension.METER
+                    "g" -> Dimension.GRAM
+                    else -> throw IllegalStateException()
+                }
+            else when (rawDimension[1]) {
+                'm' -> Dimension.METER
+                'g' -> Dimension.GRAM
+                else -> throw IllegalStateException()
+            }
 
     /**
      * Конструктор из строки. Формат строки: значение пробел размерность (1 Kg, 3 mm, 100 g и так далее).
      */
-    constructor(s: String) : this(TODO(), TODO())
+    constructor(s: String) : this(s.split(" ")[0].toDouble(), s.split(" ")[1])
 
     /**
      * Сложение с другой величиной. Если базовая размерность разная, бросить IllegalArgumentException
      * (нельзя складывать метры и килограммы)
      */
-    operator fun plus(other: DimensionalValue): DimensionalValue = TODO()
+    operator fun plus(other: DimensionalValue): DimensionalValue {
+        if (this.dimension != other.dimension) throw IllegalArgumentException()
+        return DimensionalValue(this.value + other.value, this.dimension.abbreviation)
+    }
 
     /**
      * Смена знака величины
      */
-    operator fun unaryMinus(): DimensionalValue = TODO()
+    operator fun unaryMinus(): DimensionalValue = DimensionalValue(-this.value, this.dimension.abbreviation)
 
     /**
      * Вычитание другой величины. Если базовая размерность разная, бросить IllegalArgumentException
      */
-    operator fun minus(other: DimensionalValue): DimensionalValue = TODO()
+    operator fun minus(other: DimensionalValue): DimensionalValue = this + (-other)
 
     /**
      * Умножение на число
      */
-    operator fun times(other: Double): DimensionalValue = TODO()
+    operator fun times(other: Double): DimensionalValue =
+        DimensionalValue(this.value * other, this.dimension.abbreviation)
 
     /**
      * Деление на число
      */
-    operator fun div(other: Double): DimensionalValue = TODO()
+    operator fun div(other: Double): DimensionalValue =
+        DimensionalValue(this.value / other, this.dimension.abbreviation)
 
     /**
      * Деление на другую величину. Если базовая размерность разная, бросить IllegalArgumentException
      */
-    operator fun div(other: DimensionalValue): Double = TODO()
+    operator fun div(other: DimensionalValue): Double {
+        if (this.dimension != other.dimension) throw IllegalArgumentException()
+        return this.value / other.value
+    }
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is DimensionalValue &&
+            this.dimension == other.dimension &&
+            abs(this.value - other.value) < Double.MIN_VALUE
 
     /**
      * Сравнение на больше/меньше. Если базовая размерность разная, бросить IllegalArgumentException
      */
-    override fun compareTo(other: DimensionalValue): Int = TODO()
+    override fun compareTo(other: DimensionalValue): Int {
+        if (this.dimension != other.dimension) throw IllegalArgumentException()
+        return this.value.compareTo(other.value)
+    }
+
+    override fun hashCode(): Int = 7 * this.value.hashCode() + 31 * this.dimension.abbreviation.hashCode()
 }
 
 /**
